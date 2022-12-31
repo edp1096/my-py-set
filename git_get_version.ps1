4f7c5a558ae70512411d9c962935dd48be9bdd7b
Robert Sung-wook Shin <edp1096@users.noreply.github.com>
2022-12-31 14:52:42 +0900

    Update git_get_version.ps1

--- git_get_version.ps1
+++ git_get_version.ps1
@@ -5,7 +5,8 @@ $json = Invoke-WebRequest -UseBasicParsing -Uri $uri | ConvertFrom-Json
 $versions = $json.name.Split()
 $version = $versions[$versions.length - 1]
 
-$asset = $json.assets | Where-Object { $_.name -eq "MinGit-$version-busybox-64-bit.zip" }
+#$asset = $json.assets | Where-Object { $_.name -eq "MinGit-$version-busybox-64-bit.zip" }
+$asset = $json.assets | Where-Object { $_.name -match "busybox-64-bit.zip" }
 
 $downloadURL = $asset.browser_download_url
 Write-Output $downloadURL | Out-File -FilePath "GIT_DOWNLOAD_URL" -encoding ASCII
