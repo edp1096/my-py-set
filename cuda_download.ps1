0850a45c46fba9c2f44489396e954d69c08f3a25
edp1096 <edp1096@naver.com>
2025-03-08 18:14:12 +0900

    misc.

--- cuda_download.ps1
+++ cuda_download.ps1
@@ -5,14 +5,14 @@ cd cuda
 
 import-module bitstransfer
 
-start-bitstransfer -destination cudart.zip https://developer.download.nvidia.com/compute/cuda/redist/cuda_cudart/windows-x86_64/cuda_cudart-windows-x86_64-11.6.55-archive.zip
-start-bitstransfer -destination cublas.zip https://developer.download.nvidia.com/compute/cuda/redist/libcublas/windows-x86_64/libcublas-windows-x86_64-11.10.1.25-archive.zip
-start-bitstransfer -destination cufft.zip https://developer.download.nvidia.com/compute/cuda/redist/libcufft/windows-x86_64/libcufft-windows-x86_64-10.7.2.50-archive.zip
-start-bitstransfer -destination curand.zip https://developer.download.nvidia.com/compute/cuda/redist/libcurand/windows-x86_64/libcurand-windows-x86_64-10.2.10.91-archive.zip
-start-bitstransfer -destination cusolver.zip https://developer.download.nvidia.com/compute/cuda/redist/libcusolver/windows-x86_64/libcusolver-windows-x86_64-11.4.0.1-archive.zip
-start-bitstransfer -destination cusparse.zip https://developer.download.nvidia.com/compute/cuda/redist/libcusparse/windows-x86_64/libcusparse-windows-x86_64-11.7.3.50-archive.zip
-start-bitstransfer -destination cudnn.zip https://developer.download.nvidia.com/compute/redist/cudnn/v8.4.1/local_installers/11.6/cudnn-windows-x86_64-8.4.1.50_cuda11.6-archive.zip
-start-bitstransfer -destination nvcc.zip https://developer.download.nvidia.com/compute/cuda/redist/cuda_nvcc/windows-x86_64/cuda_nvcc-windows-x86_64-11.7.99-archive.zip
+start-bitstransfer -destination cudart.zip https://developer.download.nvidia.com/compute/cuda/redist/cuda_cudart/windows-x86_64/cuda_cudart-windows-x86_64-12.6.77-archive.zip
+start-bitstransfer -destination cublas.zip https://developer.download.nvidia.com/compute/cuda/redist/libcublas/windows-x86_64/libcublas-windows-x86_64-12.6.4.1-archive.zip
+start-bitstransfer -destination cufft.zip https://developer.download.nvidia.com/compute/cuda/redist/libcufft/windows-x86_64/libcufft-windows-x86_64-11.3.0.4-archive.zip
+start-bitstransfer -destination curand.zip https://developer.download.nvidia.com/compute/cuda/redist/libcurand/windows-x86_64/libcurand-windows-x86_64-10.3.7.77-archive.zip
+start-bitstransfer -destination cusolver.zip https://developer.download.nvidia.com/compute/cuda/redist/libcusolver/windows-x86_64/libcusolver-windows-x86_64-11.7.1.2-archive.zip
+start-bitstransfer -destination cusparse.zip https://developer.download.nvidia.com/compute/cuda/redist/libcusparse/windows-x86_64/libcusparse-windows-x86_64-12.5.4.2-archive.zip
+start-bitstransfer -destination cudnn.zip https://developer.download.nvidia.com/compute/redist/cudnn/v8.7.0/local_installers/11.8/cudnn-windows-x86_64-8.7.0.84_cuda11-archive.zip
+start-bitstransfer -destination nvcc.zip https://developer.download.nvidia.com/compute/cuda/redist/cuda_nvcc/windows-x86_64/cuda_nvcc-windows-x86_64-12.6.85-archive.zip
 
 
 $files = Get-ChildItem -Path "./" -Filter "*.zip"
